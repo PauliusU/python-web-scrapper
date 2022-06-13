fec109f3ed6e584f42d119d213a93df98d11d690
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-06-14 00:31:52 +0300

    fix #1

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@ git clone https://github.com/PauliusU/python-web-scrapper.git
 cd python-web-scrapper/
 pip install --upgrade pipenv --user
 pipenv install
-pipenv shell
 cd drivers/
 curl https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_win32.zip -L -o driver.zip
 unzip driver.zip
 cd ..
-python ./src/python-web-scrapper/main.py
+pipenv run python ./src/python-web-scrapper/main.py
