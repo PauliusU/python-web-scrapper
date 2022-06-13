2a2ce10a2f06b89668b3688c331c18b629527d3a
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-06-13 22:50:26 +0300

    style: start logging messages with uppercase letter

--- src/python-web-scrapper/main.py
+++ src/python-web-scrapper/main.py
@@ -3,4 +3,4 @@ import scraper
 
 if __name__ == '__main__':
     config = config_loader.get_config()
-    scraper.scrape(config)
+    scraper.scrape_site(config)
--- src/python-web-scrapper/scraper.py
+++ src/python-web-scrapper/scraper.py
@@ -9,32 +9,33 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def scrape(config) -> None:
+def scrape_site(config) -> None:
+    """ Scrapes website using provided settings """
     try:
-        logging.info(f"loading driver path from config")
+        logging.info(f"Loading driver path from config")
         project_root = Path(__file__).parent.parent.parent
         driver_path = os.path.join(project_root, 'drivers',
                                    config["scraping"]["driver_path"])
 
-        logging.info(f"setting up driver")
+        logging.info(f"Setting up webdriver")
         service = ChromeService(driver_path)
         options = webdriver.ChromeOptions()
 
         with webdriver.Chrome(service=service, options=options) as driver:
-            logging.info(f'getting {config["scraping"]["url"]}')
+            logging.info(f'Getting {config["scraping"]["url"]}')
             driver.get(config["scraping"]["url"])
 
-            logging.info(f"loading selectors from from config")
+            logging.info(f"Loading selectors from from config")
             selector_title = config["scraping"]["selector_title"]
             selector_announcement = config["scraping"]["selector_announcement"]
 
-            logging.info(f"waiting for the page to load")
+            logging.info(f"Waiting for the page to load")
             logging.info(driver.title)
             delay = 20
             WebDriverWait(driver, delay).until(
                 expected_conditions.text_to_be_present_in_element(
                     (By.CSS_SELECTOR, selector_title),
-                    "Tesla Naudoti automobiliai"
+                    config["scraping"]["page_title_text"]
                 )
             )
 
@@ -48,4 +49,4 @@ def scrape(config) -> None:
             for phone in owner_phones:
                 print(f"phone : {phone}")
     except Exception as err:
-        logging.error(f'scrapping error: {err}')
+        logging.error(f'Scrapping error {err}')
